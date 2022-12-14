5c1dcb5d329b62d9c8ac7751cea71420b400019d
suvanaakter <120595573+suvanaakter@users.noreply.github.com>
2022-12-15 01:11:10 +0600

    Add files via upload

---
+++ math 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+   int price=20;
+   printf("price=%d\n",price);
+
+   float temparature=30.5;
+   printf("temparature=%f\n",temparature);
+
+   double temparature2=25.4;
+   printf("tem2=%lf\n",temparature2);
+
+   char character='o';
+   printf("char=%c\n",character);
+
+   return 0;
+}
